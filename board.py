1a85d7d08b8cbeab0fe0048f113164ae50caa780
Chrome_Dino <153441929+Martixi@users.noreply.github.com>
2026-03-10 17:25:24 +0100

    Added board class, main is clean now

--- board.py
+++ board.py
@@ -1,18 +1,53 @@
+from settings import *
 
-boards = [ [2, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 2],
-           [5, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3],
-           [5, 2, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 2, 3],
-           [5, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 3],
-           [5, 2, 2, 2, 1, 1, 2, 1, 1, 1, 2, 1, 1, 2, 2, 2, 3],
-           [5, 1, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 1, 3],
-           [5, 2, 2, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 2, 2, 3],
-           [5, 1, 2, 1, 1, 2, 1, 2, 2, 2, 1, 2, 1, 1, 2, 1, 3],
-           [5, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1, 3],
-           [5, 1, 2, 1, 1, 2, 1, 2, 2, 2, 1, 2, 1, 1, 2, 1, 3],
-           [5, 2, 2, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 2, 2, 3],
-           [5, 1, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 1, 3],
-           [5, 2, 2, 2, 1, 1, 2, 1, 1, 1, 2, 1, 1, 2, 2, 2, 3],
-           [5, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 3],
-           [5, 2, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 2, 3],
-           [5, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3],
-           [2, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 2] ]
+
+class Board:
+    def __init__(self, level_data):
+        self.level = level_data
+        self.tile_size = min(WIDTH / len(self.level[0]), HEIGHT / len(self.level))
+        self.offset_x = (WIDTH - (len(self.level[0]) * self.tile_size)) / 2
+        self.offset_y = (HEIGHT - (len(self.level) * self.tile_size)) / 2
+
+        # Creating surface
+        self.surface = pygame.Surface((WIDTH, HEIGHT))
+        self.surface.fill(BG_COLOR)  # Background color
+        self._pre_render_board()
+
+    def _pre_render_board(self):
+        line_size = 1
+        for i in range(len(self.level)):
+            for j in range(len(self.level[i])):
+                tile_x = int(j * self.tile_size + self.offset_x)
+                tile_y = int(i * self.tile_size + self.offset_y)
+                t_size = int(self.tile_size)
+
+                if self.level[i][j] == 1:
+                    pygame.draw.rect(self.surface, BOARD_COLOR, (tile_x, tile_y, t_size, t_size), line_size)
+                elif self.level[i][j] == 3:
+                    pygame.draw.line(self.surface, BOARD_COLOR, (tile_x, tile_y), (tile_x, tile_y + t_size), line_size)
+                elif self.level[i][j] == 4:
+                    pygame.draw.line(self.surface, BOARD_COLOR, (tile_x, tile_y), (tile_x + t_size, tile_y), line_size)
+                elif self.level[i][j] == 5:
+                    pygame.draw.line(self.surface, BOARD_COLOR, (tile_x + t_size - line_size, tile_y),
+                                     (tile_x + t_size - line_size, tile_y + t_size), line_size)
+                elif self.level[i][j] == 6:
+                    pygame.draw.line(self.surface, BOARD_COLOR, (tile_x, tile_y + t_size - line_size),
+                                     (tile_x + t_size, tile_y + t_size - line_size), line_size)
+
+    def draw(self, screen):
+        screen.blit(self.surface, (0, 0))
+
+    def check_position(self, center, player_size):
+        new_move_allowed = [False, False, False, False]
+        grid_x, grid_y = center[0] - self.offset_x, center[1] - self.offset_y
+
+        # Hitbox
+        fudge = player_size / 2
+
+        for i, (dx, dy) in enumerate([(-fudge, 0), (fudge, 0), (0, -fudge), (0, fudge)]):
+            row = int((grid_y + dy) // self.tile_size)
+            col = int((grid_x + dx) // self.tile_size)
+            if 0 <= row < len(self.level) - 1 and 0 <= col < len(self.level[0]) - 1 and row > 0 and col > 0 and \
+                    self.level[row][col] != 1:
+                new_move_allowed[i] = True
+        return new_move_allowed
--- boomit.py
+++ boomit.py
@@ -1,4 +1,4 @@
-from board import boards
+from board import Board
 from settings import *
 from player import Player
 
@@ -25,80 +25,21 @@ except pygame.error:
     print("Nie znaleziono pliku muzycznego, gra uruchomi się bez dźwięku.")
 
 # board logic
-level = boards
-tile_size = min(WIDTH / len(level[0]), HEIGHT / len(level))
+game_board = Board(BOARD)
 
-start_x = (WIDTH - (len(level[0]) * tile_size)) / 2 + tile_size
-start_y = (HEIGHT - (len(level) * tile_size)) / 2 + tile_size
-player = Player(start_x, start_y, tile_size)
+start_x = game_board.offset_x + game_board.tile_size
+start_y = game_board.offset_y + game_board.tile_size
+player = Player(start_x, start_y, game_board.tile_size)
 
 game_state = "shaking"
 
 # Functions
-# drawing board
-def draw_board(tile):
-    offset_x = (WIDTH - (len(level[0]) * tile)) / 2
-    offset_y = (HEIGHT - (len(level) * tile)) / 2
-
-    line_size = 1
-
-    for i in range(len(level)):
-        for j in range(len(level[i])):
-            tile_x = int(j * tile + offset_x)
-            tile_y = int(i * tile + offset_y)
-            t_size = int(tile)
-
-            # Case 1: Full Square
-            if level[i][j] == 1:
-                pygame.draw.rect(screen, BOARD_COLOR, (tile_x, tile_y, t_size, t_size), line_size)
-
-            # Case 3: Vertical line on the LEFT side of the tile
-            elif level[i][j] == 3:
-                pygame.draw.line(screen, BOARD_COLOR,
-                                 (tile_x, tile_y),
-                                 (tile_x, tile_y + t_size), line_size)
-
-            # Case 4: Horizontal line on the TOP side of the tile
-            elif level[i][j] == 4:
-                pygame.draw.line(screen, BOARD_COLOR,
-                                 (tile_x, tile_y),
-                                 (tile_x + t_size, tile_y), line_size)
-
-            # Case 5: Vertical line on the RIGHT side of the tile
-            elif level[i][j] == 5:
-                pygame.draw.line(screen, BOARD_COLOR,
-                                 (tile_x + t_size - line_size, tile_y),
-                                 (tile_x + t_size - line_size, tile_y + t_size), line_size)
-
-            # Case 6: Horizontal line on the BOTTOM side of the tile
-            elif level[i][j] == 6:
-                pygame.draw.line(screen, BOARD_COLOR,
-                                 (tile_x, tile_y + t_size - line_size),
-                                 (tile_x + t_size, tile_y + t_size - line_size), line_size)
-
-# collision mechanic
-def check_position(center):
-    new_move_allowed = [False, False, False, False]
-    offset_x = (WIDTH - (len(level[0]) * tile_size)) / 2
-    offset_y = (HEIGHT - (len(level) * tile_size)) / 2
-    grid_x, grid_y = center[0] - offset_x, center[1] - offset_y
-
-    fudge = 20 # idk TODO naprawić to jak sie w ściany wbija
-
-    for i, (dx, dy) in enumerate([(-fudge, 0), (fudge, 0), (0, -fudge), (0, fudge)]):
-        row = int((grid_y + dy) // tile_size)
-        col = int((grid_x + dx) // tile_size)
-        if 0 <= row < len(level) - 1 and 0 <= col < len(level[0]) - 1 and row > 0 and col > 0 and level[row][col] != 1:
-            new_move_allowed[i] = True
-    return new_move_allowed
-
-# game loop
 running = True
 while running:
     timer.tick(FPS)
-    screen.fill(BG_COLOR)
-    draw_board(tile_size)
+    game_board.draw(screen)
     is_moving = False
+
 
     if game_state == "shaking":
         if player.shake_timer >= SHAKE_DURATION:
@@ -109,13 +50,10 @@ while running:
             game_state = "playing"
 
     elif game_state == "playing":
-        # Checking for collisions
         player_center = (player.x + player.size / 2, player.y + player.size / 2)
-        move_allowed = check_position(player_center)
-        # Moving player
+        move_allowed = game_board.check_position(player_center, player.size)
         is_moving = player.move(move_allowed)
 
-    # Class decides on animation style
     player.draw(screen, game_state, is_moving)
 
     for event in pygame.event.get():
