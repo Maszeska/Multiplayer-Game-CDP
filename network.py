e5c500f838e032aa9bc3f9bf3706618b177aed5e
Chrome_Dino <153441929+Martixi@users.noreply.github.com>
2026-03-14 16:26:38 +0100

    Radmin connection test 1

--- network.py
+++ network.py
@@ -5,7 +5,7 @@ import pickle
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = "192.168.68.59"
+        self.server = "26.219.166.236"
         self.port = 5555
         self.addr = (self.server, self.port)
 
--- network_server.py
+++ network_server.py
@@ -2,7 +2,7 @@ import pickle
 import socket
 import threading
 
-server = "192.168.68.59"
+server = "0.0.0.0"
 port = 5555
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
